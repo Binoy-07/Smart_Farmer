c6d1e1163fbffa8fb4905a43eac5c33d6c2154fe
Avirup-Rakshit <aviruprakshit803@gmail.com>
2024-05-31 19:51:49 +0600

    changes in admin table

--- database/migrations/2024_05_24_154923_create__admin.php
+++ database/migrations/2024_05_24_154923_create__admin.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('_admin', function (Blueprint $table) {
             $table->id();
-            $table->string('name',255);
-            $table->string('email',255);
-            $table->string('pass',255);
+            $table->string('name:',255);
+            $table->string('email:',255);
+            $table->string('pass:',255);
 
             $table->timestamps();
         });
