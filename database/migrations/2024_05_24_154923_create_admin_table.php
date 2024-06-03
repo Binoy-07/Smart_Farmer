1bb52534b50ffab6a25e4ca7cf209f0ccd0f3ee3
Binoy-07 <csebinoy@gmail.com>
2024-06-04 00:33:42 +0600

    changes in admin table & doing colum droping

--- database/migrations/2024_05_24_154923_create_admin_table.php
+++ database/migrations/2024_05_24_154923_create_admin_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_admin', function (Blueprint $table) {
+        Schema::create('admin', function (Blueprint $table) {
             $table->id();
             $table->string('name:',255);
             $table->string('email:',255);
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('_admin');
+        Schema::dropIfExists('admin');
     }
 };
